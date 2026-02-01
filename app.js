ce1a5658f39bd4d30f0e39c852c23529e5bc58b2
Ajuu01 <raieajuna1005@gmail.com>
2026-02-02 00:02:09 +0545

    made image visible to users in browser

--- app.js
+++ app.js
@@ -20,23 +20,32 @@ app.get("/",(req,res)=>{
 
 app.post("/blog",upload.single('image'), async(req,res)=>{
     const{title,subtitle,description,image}=req.body
-    // if(!title || !subtitle || !description || !image){
-    //     return res.status(400).json({
-    //         message:"No empty field allowed."
-    //     })
-    // }
-    // await Blog.create({
-    //     title:title,
-    //     subtitle:subtitle,
-    //     description:description,
-    //     image:image
-    // })
+    const filename=req.file.filename
+    if(!title || !subtitle || !description){
+        return res.status(400).json({
+            message:"No empty field allowed."
+        })
+    }
+    await Blog.create({
+        title:title,
+        subtitle:subtitle,
+        description:description,
+        image:filename
+    })
     res.status(200).json({
         message:"Blog api hit successfully"
     })
 
 })
 
+app.get('/blog',async(req,res)=>{
+    const blogs=await Blog.find();
+    res.status(200).json({
+        message:("Blogs fetched successfully"),
+        data:blogs
+    })
+    app.use(express.static('./storage'))
+})
 app.listen((process.env.PORT),()=>{
     console.log("NODEJS Project has started.")
 })
