b0e7e29ad949cafc05d71c38a5f39dc5a2de1478
Ajuu01 <raieajuna1005@gmail.com>
2026-02-19 18:50:14 +0545

    image link

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.post("/blog",upload.single('image'), async (req,res)=>{
    const {title,subtitle,description} = req.body 
    let filename;
    if(req.file){
-     filename = "https://mern-3-0-1.onrender.com/" + req.file.filename 
+     filename = req.file.filename 
    }else{
     filename = "https://cdn.mos.cms.futurecdn.net/i26qpaxZhVC28XRTJWafQS-1200-80.jpeg"
    }
@@ -230,7 +230,7 @@ app.patch('/blog/:id',upload.single('image'), async(req,res)=>{
     const {title,subtitle,description} = req.body 
     let imageName;
     if(req.file){
-        imageName= "https://mern-3-0-1.onrender.com/" + req.file.filename
+        imageName=  req.file.filename
         const blog = await Blog.findById(id)
         const oldImageName = blog.image
     
