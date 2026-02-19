5cec898b86283e1a9a03c5b09a3b3c34aa051634
Ajuu01 <raieajuna1005@gmail.com>
2026-02-19 18:42:49 +0545

    added gitkeep

--- app.js
+++ app.js
@@ -1,133 +1,264 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const express=require('express');
-const app=express();
+// const express=require('express');
+// const app=express();
 
-app.use(express.json())
+// app.use(express.json())
+// // app.use(express.static('./storage'))
+// // This makes files available at /storage/filename.jpg
+// // app.use('/storage', express.static('storage')); 
+
+// const connectToDatabase = require('./database/index.js');
+// const Blog = require("./model/blogModel");
+// const fs=require('fs')
+// const cors=require('cors')
+// connectToDatabase();
+
+// const{multer,storage}=require('./middleware/multerConfig')
+// const upload=multer({storage:storage})
+
+// app.use(cors(
+//     {
+//         origin:["http://localhost:5173","https://blog-beige-two-18.vercel.app"]
+//     }
+// ))
+// app.get("/",(req,res)=>{
+//     res.status(200).json({
+//         message:"Hello from Here"
+//     });
+// })
+
+// app.post("/blog",upload.single('image'), async(req,res)=>{
+//     const{title,subtitle,description,image}=req.body
+//     // const filename=req.file.filename
+//     let filename;
+//     if(req.file){
+//         filename="https://mern-3-0-1.onrender.com/"+req.file.filename
+//     }
+//     else{
+//         filename="image.png"
+//     }
+//     if(!title || !subtitle || !description){
+//         return res.status(400).json({
+//             message:"No empty field allowed."
+//         })
+//     }
+//     await Blog.create({
+//         title:title,
+//         subtitle:subtitle,
+//         description:description,
+//         image:filename
+//     })
+//     res.status(200).json({
+//         message:"Blog api hit successfully"
+//     })
+
+// })
+
+// app.get('/blog',async(req,res)=>{
+//     const blogs=await Blog.find();
+//     res.status(200).json({
+//         message:("Blogs fetched successfully"),
+//         data:blogs
+//     })
+// })
+
+// app.get('/blog/:id',async(req,res)=>{
+//     const id=req.params.id
+//     const blog=await Blog.findById(id)
+//     if(!blog){
+//         return res.status(404).json({
+//             message:"No blog found"
+//         })
+//     }
+//     res.status(200).json({
+//         message:("Blog fetched successfully"),
+//         data:blog
+//     })
+// })
+
+// app.delete("/blog/:id",async(req,res)=>{
+//     const id=req.params.id
+//     const blog=await Blog.findById(id)
+//     const imageName=blog.image
+//     await Blog.findByIdAndDelete(id)
+//     fs.unlink(`storage/${imageName}`,(err)=>{
+//         if(err){
+//             console.log("Error")
+//         }
+//         else{
+//             console.log("Image deleted successfully.")
+//         }
+//     })
+//     res.status(200).json({
+//         message:"Blog deleted successfully"
+//     })
+// })
+
+// app.patch("/blog/:id",upload.single('image'),async(req,res)=>{
+//     const id=req.params.id
+//     // const imgName=req.file.filename
+//     const{title,subtitle,description}=req.body
+//     const blog=await Blog.findById(id)
+//     const imageName=blog.image
+//     let imgName
+//     if(req.file){
+//         imgName="https://mern-3-0-1.onrender.com/"+req.file.filename
+//     }
+//     else{
+//         imgName=imageName
+//     }
+//     await Blog.findByIdAndUpdate(id,{
+//         title:title,
+//         subtitle:subtitle,
+//         description:description,
+//         image:imgName
+//     })
+//     fs.unlink(`storage/${imageName}`,(err)=>{
+//         if(err){
+//             console.log("Error")
+//         }
+//         else{
+//             console.log("Image deleted successfully.")
+//         }
+//     })
+//     res.status(200).json({
+//         message:"Updated Successfully"
+//     })
+// })
 // app.use(express.static('./storage'))
-// This makes files available at /storage/filename.jpg
-// app.use('/storage', express.static('storage')); 
+// app.listen((process.env.PORT),()=>{
+//     console.log("NODEJS Project has started.")
+// })
 
-const connectToDatabase = require('./database/index.js');
-const Blog = require("./model/blogModel");
-const fs=require('fs')
-const cors=require('cors')
-connectToDatabase();
+require('dotenv').config()
+const express = require('express')
+const connectToDatabase = require('./database')
 
-const{multer,storage}=require('./middleware/multerConfig')
-const upload=multer({storage:storage})
+const app = express() 
+app.use(express.json())
+const {multer,storage} = require('./middleware/multerConfig')
+const Blog = require('./model/blogModel')
+const upload = multer({storage : storage })
+const fs = require('fs')
+const cors = require('cors')
 
 app.use(cors(
     {
         origin:["http://localhost:5173","https://blog-beige-two-18.vercel.app"]
     }
 ))
+
+connectToDatabase()
+
 app.get("/",(req,res)=>{
     res.status(200).json({
-        message:"Hello from Here"
-    });
+        hello : "This is home page"
+    })
 })
 
-app.post("/blog",upload.single('image'), async(req,res)=>{
-    const{title,subtitle,description,image}=req.body
-    // const filename=req.file.filename
-    let filename;
-    if(req.file){
-        filename="https://mern-3-0-1.onrender.com/"+req.file.filename
-    }
-    else{
-        filename="image.png"
-    }
-    if(!title || !subtitle || !description){
+app.post("/blog",upload.single('image'), async (req,res)=>{
+   const {title,subtitle,description} = req.body 
+   let filename;
+   if(req.file){
+     filename = "https://mern-3-0-1.onrender.com/" + req.file.filename 
+   }else{
+    filename = "https://cdn.mos.cms.futurecdn.net/i26qpaxZhVC28XRTJWafQS-1200-80.jpeg"
+   }
+
+   if(!title || !subtitle || !description){
         return res.status(400).json({
-            message:"No empty field allowed."
+            message : "Please provide title,subtitle,description"
         })
-    }
-    await Blog.create({
-        title:title,
-        subtitle:subtitle,
-        description:description,
-        image:filename
-    })
+        
+   }
+   await Blog.create({
+    title : title, 
+    subtitle : subtitle, 
+    description : description, 
+    image : filename
+   })
     res.status(200).json({
-        message:"Blog api hit successfully"
-    })
-
-})
-
-app.get('/blog',async(req,res)=>{
-    const blogs=await Blog.find();
-    res.status(200).json({
-        message:("Blogs fetched successfully"),
-        data:blogs
+        message : "Blog api hit successfully"
     })
 })
 
-app.get('/blog/:id',async(req,res)=>{
-    const id=req.params.id
-    const blog=await Blog.findById(id)
+app.get("/blog",async (req,res)=>{
+   const blogs =  await Blog.find() // returns array
+   res.status(200).json({
+    message : "Blogs fetched successfully", 
+    data : blogs
+   })
+})
+
+app.get("/blog/:id",async (req,res)=>{
+    const id = req.params.id
+    const blog =  await Blog.findById(id) // object
+
     if(!blog){
         return res.status(404).json({
-            message:"No blog found"
+            message : "no data found"
         })
     }
-    res.status(200).json({
-        message:("Blog fetched successfully"),
-        data:blog
-    })
-})
 
-app.delete("/blog/:id",async(req,res)=>{
-    const id=req.params.id
-    const blog=await Blog.findById(id)
-    const imageName=blog.image
+    res.status(200).json({
+        message : "Fetched successfully", 
+        data : blog
+    })
+  
+})
+app.delete("/blog/:id",async (req,res)=>{
+    const id = req.params.id
+    const blog = await Blog.findById(id)
+    const imageName = blog.image
+ 
+    fs.unlink(`storage/${imageName}`,(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("File deleted successfully")
+        }
+    })
     await Blog.findByIdAndDelete(id)
-    fs.unlink(`storage/${imageName}`,(err)=>{
-        if(err){
-            console.log("Error")
-        }
-        else{
-            console.log("Image deleted successfully.")
-        }
-    })
     res.status(200).json({
-        message:"Blog deleted successfully"
+        message : 'Blog deleted successfully'
     })
 })
 
-app.patch("/blog/:id",upload.single('image'),async(req,res)=>{
-    const id=req.params.id
-    // const imgName=req.file.filename
-    const{title,subtitle,description}=req.body
-    const blog=await Blog.findById(id)
-    const imageName=blog.image
-    let imgName
+app.patch('/blog/:id',upload.single('image'), async(req,res)=>{
+    const id = req.params.id 
+    const {title,subtitle,description} = req.body 
+    let imageName;
     if(req.file){
-        imgName="https://mern-3-0-1.onrender.com/"+req.file.filename
+        imageName= "https://mern-3-0-1.onrender.com/" + req.file.filename
+        const blog = await Blog.findById(id)
+        const oldImageName = blog.image
+    
+        fs.unlink(`storage/${oldImageName}`,(err)=>{
+            if(err){
+                console.log(err)
+            }else{
+                console.log("File deleted successfully")
+            }
+        })
     }
-    else{
-        imgName=imageName
-    }
-    await Blog.findByIdAndUpdate(id,{
-        title:title,
-        subtitle:subtitle,
-        description:description,
-        image:imgName
-    })
-    fs.unlink(`storage/${imageName}`,(err)=>{
-        if(err){
-            console.log("Error")
-        }
-        else{
-            console.log("Image deleted successfully.")
-        }
+   await Blog.findByIdAndUpdate(id,{
+        title : title, 
+        subtitle : subtitle, 
+        description : description, 
+        image : imageName
     })
     res.status(200).json({
-        message:"Updated Successfully"
+        message : "Blog updated successfully"
     })
 })
+
+
+
 app.use(express.static('./storage'))
-app.listen((process.env.PORT),()=>{
-    console.log("NODEJS Project has started.")
+
+app.listen(process.env.PORT,()=>{
+    console.log("NodeJs project has started")
 })
 
+// mongodb+srv://digitalpathshala:<password>@cluster0.iibdr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
